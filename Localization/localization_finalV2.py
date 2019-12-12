cf347611762e701741bfebc9463d453e527dd5c9
peetermans1995 <peetermans1995@gmail.com>
2019-12-12 12:46:12 +0100

    changed localize method

---
+++ Localization/localization_finalV2.py
@@ -0,0 +1,74 @@
+import paho.mqtt.client as mqtt
+import pymongo
+import time
+import json
+
+from bitstring import ConstBitStream
+
+import numpy as np
+from pymongo import MongoClient
+
+databaseClient = MongoClient();
+database = databaseClient["FingerprintDB"]
+collection = database["RSS"]
+
+gatewayList = ["4337313400210032","433731340023003d","42373434002a0049","463230390032003e"]
+rx_values=[0,0,0,0]
+
+
+
+#X5 - Y1 --> geeft x = 4.4 y = 1.85
+# 4337313400210032
+rx_values[0] = 54
+# 433731340023003d
+rx_values[1] = 53
+# 42373434002a0049
+rx_values[2] = 51
+# 463230390032003e
+rx_values[3] = 50
+
+
+
+
+def localize(rx_values, k):
+    temp = []
+    for document in collection.find():
+        diff = []
+        for gateway_id in gatewayList:
+            if gateway_id == '4337313400210032':
+                diff.append(int(rx_values[0]) - int(document['GatewayInfo'][gateway_id]))
+            if gateway_id == '433731340023003d':
+                diff.append(int(rx_values[1]) - int(document['GatewayInfo'][gateway_id]))
+            if gateway_id == '42373434002a0049':
+                diff.append(int(rx_values[2]) - int(document['GatewayInfo'][gateway_id]))
+            if gateway_id == '463230390032003e':
+                diff.append(int(rx_values[3]) - int(document['GatewayInfo'][gateway_id]))
+        rms = np.sqrt(np.mean(np.square(diff)))
+        print('diff = ' + str(diff) + 'which gives an rms = ' + str(rms))
+
+        temp.append({'x': document['Location']['X'], 'y': document['Location']['Y'], 'rms': rms})
+
+    # k-nearest neighbors
+    ordered_locations = sorted(temp, key=lambda i: i['rms'])  # sort on RMS value
+    nearest_neighbors = ordered_locations[:k]
+    print('knn: ' + str(nearest_neighbors))
+
+    x_coordinate = 0
+    x_values = []
+    y_coordinate = 0
+    y_values = []
+    for fingerprint in nearest_neighbors:
+        #print fingerprint['x']
+        x_values.append(fingerprint['x'])
+        #print fingerprint['y']
+        y_values.append(fingerprint['y'])
+
+    x_coordinate = np.average(x_values)
+    y_coordinate = np.average(y_values)
+    print str(x_values) + 'the average coordinate of x is ' + str(x_coordinate)
+    print str(y_values) + 'the average coordinate of y is ' + str(y_coordinate)
+
+    return {'x': x_coordinate, 'y': y_coordinate}
+
+
+#print localize (rx_values , 7) # make sure that the input of rx_values is in the correct order!
